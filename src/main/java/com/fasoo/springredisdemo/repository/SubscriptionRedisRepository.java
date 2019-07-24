02221bd0c9785e872a68a37f76f04286d175b14e
jinbeomdev <niutn@naver.com>
2019-07-25 07:25:49 +0900

    구독 repo 등록

--- src/main/java/com/fasoo/springredisdemo/repository/SubscriptionRedisRepository.java
+++ src/main/java/com/fasoo/springredisdemo/repository/SubscriptionRedisRepository.java
@@ -1,9 +1,9 @@
 package com.fasoo.springredisdemo.repository;
 
-import com.fasoo.springredisdemo.domain.Point;
+import com.fasoo.springredisdemo.domain.Subscription;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PointRedisRepository extends CrudRepository<Point, String> {
+public interface SubscriptionRedisRepository extends CrudRepository<Subscription, Long> {
 }
