17a15cfe1a50851167a0d721bcc693761c6f7c2b
jinbeomdev <niutn@naver.com>
2019-07-25 15:18:14 +0900

    이름 변경

--- src/main/java/com/fasoo/springredisdemo/repository/SubscriptionRedisRepository.java
+++ src/main/java/com/fasoo/springredisdemo/repository/SubscriptionRedisRepository.java
@@ -1,9 +1,9 @@
 package com.fasoo.springredisdemo.repository;
 
-import com.fasoo.springredisdemo.domain.Subscription;
+import com.fasoo.springredisdemo.domain.SubscriptionDomain;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface SubscriptionRedisRepository extends CrudRepository<Subscription, Long> {
+public interface SubscriptionRedisRepository extends CrudRepository<SubscriptionDomain, Long> {
 }
